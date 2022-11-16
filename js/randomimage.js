07c9c8053ac7876cedd4c62ae6829cbda80edd69
etorger <etorger@tktk.ee>
2022-11-16 16:53:23 +0200

    jss

---
+++ js/randomimage.js
@@ -0,0 +1,12 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["img1.jpeg", "img2.jpeg", "img3.jpeg", "img4.jpeg"];
+
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage() {
+    let randomIndex = Math.floor(Math.random() * images.length);
+    console.log(randomIndex);
+    image.src = "images/"+ images [randomIndex]
+}
